0053b3e0ee01a57c3491252602fcf0fd039b39f4
Hang Jiang <hjian42@icloud.com>
2019-04-25 21:20:36 -0700

    test modify

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from crawl import *
 import json
-
+i = 0
 with open('UK.json') as f:
 	print('------------------')
 	for line in f:
 		data = json.loads(line)
 		if not has_url(data['text']) and is_normal_user(data['user']):
 			print(json.dumps(data['text'], indent=4))
+		i += 1
+print("Num of Tweets:\t", str(i))
