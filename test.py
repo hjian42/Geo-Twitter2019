a4bb26601345109e5874e697cd7084310682820c
Hang Jiang <hjian42@icloud.com>
2019-04-28 01:00:21 -0700

    change test file

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ i = 0
 with open('UK.json') as f:
 	print('------------------')
 	for line in f:
-		data = json.loads(line)
-		if not has_url(data['text']) and is_normal_user(data['user']):
-			print(json.dumps(data['text'], indent=4))
+		# data = json.loads(line)
+		# if not has_url(data['text']) and is_normal_user(data['user']):
+		# 	print(json.dumps(data['text'], indent=4))
 		i += 1
 print("Num of Tweets:\t", str(i))
